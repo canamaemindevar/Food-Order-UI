59e5e1051e2d0e3236037b795ce30680bd1607cd
canamaemindevar <94639136+canamaemindevar@users.noreply.github.com>
2023-10-18 17:50:04 +0300

    Initial Commit

---
+++ Food Order UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food Order UI
+//
+//  Created by Emincan Antalyalı on 18.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
